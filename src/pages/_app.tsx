842944c415d3b527cb3c3731a0cafafd2b004b8d
Daniel Pena <0612332@gmail.com>
2026-06-03 18:55:45 -0400

    Klaviyo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,22 @@
 import '@/styles/globals.scss';
 import { FaustProvider } from '@faustwp/core';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { CartProvider } from '@/context/CartContext';
+
+const klaviyoPublicKey = process.env.NEXT_PUBLIC_KLAVIYO_PUBLIC_KEY;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <FaustProvider pageProps={pageProps}>
       <CartProvider>
+        {klaviyoPublicKey && (
+          <Script
+            id="klaviyo-onsite"
+            strategy="afterInteractive"
+            src={`https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=${klaviyoPublicKey}`}
+          />
+        )}
         <Component {...pageProps} />
       </CartProvider>
     </FaustProvider>
