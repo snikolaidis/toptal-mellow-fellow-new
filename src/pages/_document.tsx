3c1059cb8084f72213387d0d8d25cae7812f92d7
Mohammed Mansour <79015324+Mohammed-blue@users.noreply.github.com>
2026-06-30 12:01:28 -0400

    Use single Klaviyo account for forms and reviews

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,6 @@ import { getBodyClass } from '@/lib/bodyClass';
 
 const awinAdvertiserId = process.env.NEXT_PUBLIC_AWIN_ADVERTISER_ID ?? '';
 const klaviyoPublicKey = process.env.NEXT_PUBLIC_KLAVIYO_PUBLIC_KEY;
-const klaviyoReviewsPublicKey = process.env.NEXT_PUBLIC_KLAVIYO_REVIEWS_PUBLIC_KEY;
 const yotpoLoyaltyLoader = process.env.NEXT_PUBLIC_YOTPO_LOYALTY_LOADER;
 const aioaToken = process.env.NEXT_PUBLIC_AIOA_TOKEN ?? '';
 const aioaColor = process.env.NEXT_PUBLIC_AIOA_COLOR ?? '000000';
@@ -11,9 +10,6 @@ const aioaPosition = process.env.NEXT_PUBLIC_AIOA_POSITION ?? 'bottom_left';
 const aioaWidgetSrc = `https://www.skynettechnologies.com/accessibility/js/all-in-one-accessibility-js-widget-minify.js?colorcode=${aioaColor}&token=${aioaToken}&position=${aioaPosition}`;
 const klaviyoEnabled =
   !!klaviyoPublicKey &&
-  (process.env.NODE_ENV === 'production' || process.env.NEXT_PUBLIC_KLAVIYO_DEV === 'true');
-const klaviyoReviewsEnabled =
-  !!klaviyoReviewsPublicKey &&
   (process.env.NODE_ENV === 'production' || process.env.NEXT_PUBLIC_KLAVIYO_DEV === 'true');
 
 export default function Document({ bodyClass }: { bodyClass: string }) {
@@ -39,13 +35,6 @@ export default function Document({ bodyClass }: { bodyClass: string }) {
             type="text/javascript"
             async
             src={`https://static.klaviyo.com/onsite/js/${klaviyoPublicKey}/klaviyo.js`}
-          />
-        )}
-        {klaviyoReviewsEnabled && (
-          <script
-            type="text/javascript"
-            async
-            src={`https://static.klaviyo.com/onsite/js/${klaviyoReviewsPublicKey}/klaviyo.js`}
           />
         )}
         {yotpoLoyaltyLoader && (
