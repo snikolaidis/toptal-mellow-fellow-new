7c208025d59eb93a835a6c2c822607b985c89277
hanan <105388444+HananAbuKwaider@users.noreply.github.com>
2026-08-28 17:18:06 +0300

    upselling props was missing from the query

--- src/scripts/sync-products.ts
+++ src/scripts/sync-products.ts
@@ -53,6 +53,20 @@ const PRODUCT_QUERY = `
           singleCannabinoid { nodes { name slug } }
           mG { nodes { name slug } }
           pieces { nodes { name slug } }
+          uniqueSellingProps {
+            nodes {
+              id
+              name
+              uniqueSellingFields {
+                propIcon {
+                  node {
+                    sourceUrl
+                    altText
+                  }
+                }
+              }
+            }
+          }
         }
         ... on VariableProduct {
           id databaseId name slug type
@@ -74,6 +88,20 @@ const PRODUCT_QUERY = `
           singleCannabinoid { nodes { name slug } }
           mG { nodes { name slug } }
           pieces { nodes { name slug } }
+          uniqueSellingProps {
+            nodes {
+              id
+              name
+              uniqueSellingFields {
+                propIcon {
+                  node {
+                    sourceUrl
+                    altText
+                  }
+                }
+              }
+            }
+          }
         }
       }
     }
