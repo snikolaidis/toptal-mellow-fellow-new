c533421fddea4cf28b2681cbd3e434fe9b9e8615
Stratos Nikolaidis <snikolaidis@gmail.com>
2026-09-12 09:37:28 +0200

    Typo fixes

--- src/components/checkout/BillingStep.tsx
+++ src/components/checkout/BillingStep.tsx
@@ -396,7 +396,7 @@ export default function BillingStep({
               className={styles.continueButton}
               onClick={onContinue}
             >
-              Continue to Payment
+              Continue to Age Verification
             </button>
           </div>
         </main>
